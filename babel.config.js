7829ff26926bcde267b936fe7b79e728d74fb184
Philipp Rappold <philipprappold@me.com>
2019-02-19 17:33:50 +0100

    build: Update Babel config

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,15 @@ const isTest = String(process.env.NODE_ENV) === 'test'
 
 module.exports = {
   presets: [
-    ['@babel/preset-env', { modules: isTest ? 'commonjs' : false }],
+    [
+      '@babel/preset-env',
+      {
+        modules: isTest ? 'commonjs' : false,
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
     '@babel/preset-react',
   ],
   plugins: [
