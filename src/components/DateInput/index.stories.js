90f02fab403f166a659874e6350efeb5d42c9c79
Philipp Rappold <philipprappold@me.com>
2019-04-09 14:36:13 +0200

    feat: Add DateInput component

---
+++ src/components/DateInput/index.stories.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { DateInput } from './'
+
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+]
+
+storiesOf('DateInput', module)
+  .add('basic', () => (
+    <DateInput
+      id="dob"
+      label="Date of birth"
+      months={months}
+      onChange={date => console.log(date)}
+    />
+  ))
+  .add('with initial date (string)', () => (
+    <DateInput
+      id="dob"
+      label="Date of birth"
+      months={months}
+      onChange={date => console.log(date)}
+      initialSelectedDate="1990-09-06"
+    />
+  ))
+  .add('with initial date (object)', () => (
+    <DateInput
+      id="dob"
+      label="Date of birth"
+      months={months}
+      onChange={date => console.log(date)}
+      initialSelectedDate={new Date()}
+    />
+  ))
--- src/utils.js
+++ src/utils.js
@@ -3,6 +3,10 @@ export const callAll = (...fns) => (...args) =>
 
 export const stopPropagation = e => e.stopPropagation()
 
+export function isServer() {
+  return !(typeof window !== 'undefined' && window.document)
+}
+
 export const debounce = (fn, time) => {
   let timeout
 
