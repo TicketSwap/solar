76c6b48fe1f56f4e8bc108ed2ab7178faf57e723
Rob Vermeer <rob@vermeertech.nl>
2019-07-09 18:05:27 +0200

    fix(Spinner): Change animation

--- src/components/Spinner/index.js
+++ src/components/Spinner/index.js
@@ -15,7 +15,7 @@ const rotate = keyframes`
 `
 
 const Rotate = styled.div`
-  animation: ${rotate} 1s ease-in-out infinite;
+  animation: ${rotate} 1s cubic-bezier(0.65, 0.25, 0.25, 0.75) infinite;
   display: flex;
   justify-content: center;
   align-items: center;
