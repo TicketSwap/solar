9a11cb6922c0170cb5c9a2207e991fb47f34501c
Glenn Gijsberts <glenngijsberts95@gmail.com>
2019-07-16 09:38:44 +0200

    build(deps-dev): replaced jest-dom with @testing-library/jest-dom

--- test/jest.client.js
+++ test/jest.client.js
@@ -3,7 +3,7 @@ module.exports = {
   displayName: 'dom',
   testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: [
-    'jest-dom/extend-expect',
+    '@testing-library/jest-dom/extend-expect',
     '@testing-library/react/cleanup-after-each',
     require.resolve('./setup-tests.js'),
   ],
