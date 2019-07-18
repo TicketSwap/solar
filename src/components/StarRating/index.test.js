7bdc89e7590e83a32e4aaa1b9fa60b0537c79f23
Philipp Rappold <philipprappold@me.com>
2019-07-18 15:03:00 +0200

    fix(StarRating): Fix markup and remove form-tag

--- src/components/StarRating/index.test.js
+++ src/components/StarRating/index.test.js
@@ -4,25 +4,29 @@ import { StarRating } from './'
 
 describe('StarRating', () => {
   it('renders without crashing', () => {
-    const { container } = render(<StarRating />)
+    const { container } = render(<StarRating label="Rating" />)
     expect(container.firstChild).toBeInTheDocument()
   })
 
   it('has 0 selected initially when no initial rating passed', () => {
-    const { getByLabelText } = render(<StarRating />)
+    const { getByLabelText } = render(<StarRating label="Rating" />)
     const checkbox = getByLabelText(/0 stars/i)
     expect(checkbox.checked).toEqual(true)
   })
 
   it('has 2 stars selected initially when passed as initial rating', () => {
-    const { getByLabelText } = render(<StarRating initialRating={2} />)
+    const { getByLabelText } = render(
+      <StarRating initialRating={2} label="Rating" />
+    )
     const checkbox = getByLabelText(/2 stars/i)
     expect(checkbox.checked).toEqual(true)
   })
 
   it('calls onChange handler with the selected rating', () => {
     const mockedOnChange = jest.fn()
-    const { getByLabelText } = render(<StarRating onChange={mockedOnChange} />)
+    const { getByLabelText } = render(
+      <StarRating onChange={mockedOnChange} label="Rating" />
+    )
     fireEvent.click(getByLabelText(/2 stars/i))
     expect(mockedOnChange).toHaveBeenCalledTimes(1)
     expect(mockedOnChange).toHaveBeenCalledWith(2)
