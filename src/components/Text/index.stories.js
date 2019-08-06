cb6904ea80a75376fec6460a5e9cc68da539c628
Philipp Rappold <philipprappold@me.com>
2019-08-06 12:27:42 +0200

    feat: add Text and BaseStyles components

---
+++ src/components/Text/index.stories.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { Text } from './'
+
+storiesOf('Text', module).add('basic', () => (
+  <Text>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus voluptate
+    autem eveniet distinctio dicta magni rem. Atque quaerat obcaecati,
+    cupiditate vel assumenda explicabo aliquid minima inventore tempora tenetur
+    qui modi fuga nisi unde. Quam magni soluta earum rem deleniti, repudiandae
+    sunt, aperiam eaque dolorem odit veniam numquam nulla natus quo incidunt
+    cumque corrupti. Est hic nemo sint reiciendis eum nihil aut esse deleniti
+    illum? Quia architecto nisi enim dignissimos dicta nemo provident explicabo
+    libero ab voluptatem tempora, excepturi nam dolorum dolor cum assumenda
+    nobis quod blanditiis laudantium illum, autem ex laboriosam. Aliquid facilis
+    incidunt maxime veritatis aliquam nulla similique pariatur?
+  </Text>
+))
