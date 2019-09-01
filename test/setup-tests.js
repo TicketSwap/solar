b660275388d85477ca05d09e7071946245837af8
Philipp Rappold <philipprappold@me.com>
2019-09-01 11:33:18 +0200

    feat: add useTransition hook

--- test/setup-tests.js
+++ test/setup-tests.js
@@ -1,3 +1,13 @@
+// Mock custom useTransition hook
+jest.mock('../src/hooks/useTransition', () => {
+  return {
+    useTransition: ({ on }) => ({
+      state: 'mockedState',
+      show: on,
+    }),
+  }
+})
+
 // Mock window.matchMedia
 window.matchMedia = jest.fn().mockImplementation(query => {
   return {
