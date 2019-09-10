bb983fa359117aec232e3511b95e5edc09fefeae
Philipp Rappold <philipprappold@me.com>
2019-09-10 23:29:45 +0200

    Refactor theme file

--- src/theme.ts
+++ src/theme.ts
@@ -131,20 +131,21 @@ export const easing = {
 }
 
 export const easingFunctions = {
-  linear: t => t,
-  easeInQuad: t => t * t,
-  easeOutQuad: t => t * (2 - t),
-  easeInOutQuad: t => (t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t),
-  easeInCubic: t => t * t * t,
-  easeOutCubic: t => --t * t * t + 1,
-  easeInOutCubic: t =>
+  linear: (t: number) => t,
+  easeInQuad: (t: number) => t * t,
+  easeOutQuad: (t: number) => t * (2 - t),
+  easeInOutQuad: (t: number) => (t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t),
+  easeInCubic: (t: number) => t * t * t,
+  easeOutCubic: (t: number) => --t * t * t + 1,
+  easeInOutCubic: (t: number) =>
     t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1,
-  easeInQuart: t => t * t * t * t,
-  easeOutQuart: t => 1 - --t * t * t * t,
-  easeInOutQuart: t => (t < 0.5 ? 8 * t * t * t * t : 1 - 8 * --t * t * t * t),
-  easeInQuint: t => t * t * t * t * t,
-  easeOutQuint: t => 1 + --t * t * t * t * t,
-  easeInOutQuint: t =>
+  easeInQuart: (t: number) => t * t * t * t,
+  easeOutQuart: (t: number) => 1 - --t * t * t * t,
+  easeInOutQuart: (t: number) =>
+    t < 0.5 ? 8 * t * t * t * t : 1 - 8 * --t * t * t * t,
+  easeInQuint: (t: number) => t * t * t * t * t,
+  easeOutQuint: (t: number) => 1 + --t * t * t * t * t,
+  easeInOutQuint: (t: number) =>
     t < 0.5 ? 16 * t * t * t * t * t : 1 + 16 * --t * t * t * t * t,
 }
 
