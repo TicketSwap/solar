7381ef32880f1062707ed5249f9916f575475beb
Philipp Rappold <philipprappold@me.com>
2019-09-10 23:30:36 +0200

    Add and configure TypeScript

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { BaseStyles } from '../src/components/BaseStyles'
 import { configure, addDecorator } from '@storybook/react'
 
-const req = require.context('../src/components', true, /\.stories\.js$/)
+const req = require.context('../src/components', true, /\.stories\.tsx$/)
 
 function loadStories() {
   req.keys().forEach(filename => req(filename))
