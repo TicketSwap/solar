330fea6c364e36c1da4178eda8b41c01832513fb
Philipp Rappold <philipprappold@me.com>
2019-09-11 14:12:15 +0200

    feat: add BaseButton component

---
+++ src/components/BaseButton/index.js
@@ -0,0 +1,56 @@
+import styled from '@emotion/styled'
+import { space, color, fontWeight, radius } from '../../theme'
+import { baseTextStyles } from '../Text'
+
+export const BaseButton = styled.button`
+  ${baseTextStyles};
+  position: relative;
+  color: ${p =>
+    p.disabled
+      ? color.spaceLight
+      : p.variant === 'success'
+      ? color.titan
+      : p.variant === 'danger'
+      ? color.mars
+      : p.variant === 'warning'
+      ? color.sun
+      : color.earth};
+  font-weight: ${fontWeight.semiBold};
+  cursor: ${props => (props.disabled ? 'not-allowed' : 'pointer')};
+
+  &:hover,
+  &:focus {
+    outline: 0;
+    box-shadow: none;
+    color: ${p =>
+      p.disabled
+        ? color.spaceLight
+        : p.variant === 'success'
+        ? color.titanLight
+        : p.variant === 'danger'
+        ? color.marsLight
+        : p.variant === 'warning'
+        ? color.sunLight
+        : color.earthLight};
+  }
+
+  &:focus::after {
+    content: '';
+    position: absolute;
+    left: -4px;
+    top: -2px;
+    right: -4px;
+    bottom: -2px;
+    background-color: transparent;
+    border-radius: ${radius.md};
+    box-shadow: 0 0 0 ${space[4]}
+      ${p =>
+        p.variant === 'success'
+          ? color.titanFocusAlpha
+          : p.variant === 'danger'
+          ? color.marsFocusAlpha
+          : p.variant === 'warning'
+          ? color.sunFocusAlpha
+          : color.earthFocusAlpha};
+  }
+`
