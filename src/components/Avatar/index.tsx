e5c4592888fa5c8a1a144c7c2d2ab5f09fd813a8
Lucas Feliciano <mail@lucasfeliciano.com>
2019-09-11 23:55:17 +0200

    Reorder code structure. ( READ MORE.... )
    I liked this structure a lot.
    Because you open the file you see the component type, the component and the default props right away so you know what to expect.
    
    If agreed squash with previous commit.
    If not, delete commit

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -4,6 +4,52 @@ import { css } from '@emotion/core'
 import { Image } from '../Image'
 import { color } from '../../theme'
 
+type AvatarProps = {
+  size: number
+  src?: string
+}
+
+export const Avatar: FunctionComponent<AvatarProps> = ({
+  size,
+  src,
+  ...props
+}) => (
+  <Wrapper size={size} {...props}>
+    {src ? (
+      <StyledImage src={src} alt="Avatar" data-testid="image" />
+    ) : (
+      <Placeholder />
+    )}
+  </Wrapper>
+)
+
+Avatar.defaultProps = {
+  size: 32,
+}
+
+export const Placeholder = () => (
+  <InlineSvg
+    fillRule="evenodd"
+    clipRule="evenodd"
+    strokeLinejoin="round"
+    strokeMiterlimit="1.414"
+    xmlns="http://www.w3.org/2000/svg"
+    role="presentation"
+    focusable="false"
+    viewBox="0 0 32 32"
+    preserveAspectRatio="xMidYMid meet"
+  >
+    <g>
+      <path fill="#E6F8FE" d="M0 0h32v32H0z" />
+      <path
+        d="M28.45 32H3.608c-.115-1.43.033-2.87.472-4.235C4.957 25.631 6.762 24 8.864 24h1.078c1.695 1.25 3.79 2 6.065 2 2.273 0 4.368-.75 6.063-2h1.066c2.101 0 3.907 1.631 4.784 3.765A10.58 10.58 0 0 1 28.45 32z"
+        fill="#00B2F3"
+      />
+      <path d="M23 16a7 7 0 1 1-14 0 7 7 0 0 1 14 0" fill="#00B2F3" />
+    </g>
+  </InlineSvg>
+)
+
 const Wrapper = styled.div<AvatarProps>`
   vertical-align: middle;
   display: inline-block;
@@ -37,49 +83,3 @@ const StyledImage = styled(Image)`
 const InlineSvg = styled.svg`
   ${imageStyles};
 `
-
-export const Placeholder = () => (
-  <InlineSvg
-    fillRule="evenodd"
-    clipRule="evenodd"
-    strokeLinejoin="round"
-    strokeMiterlimit="1.414"
-    xmlns="http://www.w3.org/2000/svg"
-    role="presentation"
-    focusable="false"
-    viewBox="0 0 32 32"
-    preserveAspectRatio="xMidYMid meet"
-  >
-    <g>
-      <path fill="#E6F8FE" d="M0 0h32v32H0z" />
-      <path
-        d="M28.45 32H3.608c-.115-1.43.033-2.87.472-4.235C4.957 25.631 6.762 24 8.864 24h1.078c1.695 1.25 3.79 2 6.065 2 2.273 0 4.368-.75 6.063-2h1.066c2.101 0 3.907 1.631 4.784 3.765A10.58 10.58 0 0 1 28.45 32z"
-        fill="#00B2F3"
-      />
-      <path d="M23 16a7 7 0 1 1-14 0 7 7 0 0 1 14 0" fill="#00B2F3" />
-    </g>
-  </InlineSvg>
-)
-
-type AvatarProps = {
-  size: number
-  src?: string
-}
-
-export const Avatar: FunctionComponent<AvatarProps> = ({
-  size,
-  src,
-  ...props
-}) => (
-  <Wrapper size={size} {...props}>
-    {src ? (
-      <StyledImage src={src} alt="Avatar" data-testid="image" />
-    ) : (
-      <Placeholder />
-    )}
-  </Wrapper>
-)
-
-Avatar.defaultProps = {
-  size: 32,
-}
