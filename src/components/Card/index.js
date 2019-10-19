137a07e4496549bc036dd6fac3bae26000387ac9
Philipp Rappold <philipprappold@me.com>
2019-10-19 18:48:07 +0200

    feat: New Card component

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,38 +1,190 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import { css } from '@emotion/core'
 import styled from '@emotion/styled'
-import { color, space } from '../../theme'
+import { Image } from '../Image'
+import {
+  color,
+  radius,
+  space,
+  fontSize,
+  fontWeight,
+  device,
+  lineHeight,
+} from '../../theme'
 
 const Container = styled.div`
-  white-space: normal;
-  color: ${color.space};
-`
-
-const ImageWrapper = styled.div`
   position: relative;
-  margin-bottom: ${space[8]};
+  border-radius: ${radius.lg};
+  padding: ${p =>
+    p.size === 'large' ? p.pad && space[16] : p.pad && space[12]};
+  background-color: ${color.stardust};
+  backface-visibility: hidden;
+
+  @media ${device.mobileL} {
+    padding: ${p =>
+      p.size === 'large' ? p.pad && space[24] : p.pad && space[16]};
+  }
+
+  &:hover,
+  &:focus {
+    background-color: ${color.spaceLightest};
+  }
 `
 
-const Adornment = styled.div`
-  position: absolute;
-  z-index: 2;
-  color: white;
-  left: 0;
-  top: 0;
-  right: 0;
-  bottom: 0;
+const Content = styled.div`
+  font-size: ${fontSize[20]};
+  font-weight: ${fontWeight.regular};
   display: flex;
-  justify-content: center;
-  align-items: center;
+  flex-direction: row;
+  align-items: ${props => props.alignItems};
+  justify-content: space-between;
+
+  ${props =>
+    props.floating &&
+    css`
+      padding: ${space[12]};
+      background-image: linear-gradient(
+        to bottom,
+        rgba(0, 19, 25, 0),
+        rgba(0, 19, 25, 0.8)
+      );
+      backdrop-filter: blur(0.5rem);
+      border-bottom-left-radius: ${radius.lg};
+      border-bottom-right-radius: ${radius.lg};
+      position: absolute;
+      z-index: 1;
+      left: 0;
+      bottom: 0;
+      right: 0;
+
+      @media ${device.mobileL} {
+        padding: ${space[16]};
+      }
+
+      ${Container}:hover &,
+      ${Container}:focus & {
+        background-image: linear-gradient(
+          to bottom,
+          rgba(0, 19, 25, 0.25),
+          rgba(0, 19, 25, 1)
+        );
+      }
+    `};
 `
 
-export const Card = ({ image, adornment, children, ...props }) => (
-  <Container {...props}>
-    {image && (
-      <ImageWrapper>
-        {adornment && <Adornment>{adornment}</Adornment>}
-        {image}
-      </ImageWrapper>
-    )}
-    {children}
-  </Container>
-)
+const Header = styled.header`
+  padding: 0;
+  flex-grow: 0;
+  margin-right: ${space[16]};
+`
+
+const Body = styled.div`
+  color: ${props => (props.theme === 'light' ? 'white' : color.space)};
+  flex-grow: 1;
+  text-align: left;
+  position: relative;
+  overflow: hidden;
+`
+
+const Footer = styled.footer`
+  padding: 0;
+  flex-grow: 0;
+  margin-left: ${space[16]};
+`
+
+const textStyles = css`
+  display: block;
+  line-height: ${lineHeight.title};
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
+
+  @media ${device.mobileL} {
+    line-height: ${lineHeight.solid};
+  }
+`
+
+const Title = styled.h4`
+  ${textStyles};
+  font-size: ${p => (p.size === 'large' ? fontSize[18] : fontSize[16])};
+  font-weight: ${fontWeight.semiBold};
+
+  @media ${device.mobileL} {
+    font-size: ${p => (p.size === 'large' ? fontSize[20] : fontSize[18])};
+  }
+`
+
+const Subtitle = styled.h5`
+  ${textStyles};
+  font-size: ${p => (p.size === 'large' ? fontSize[16] : fontSize[14])};
+  font-weight: ${fontWeight.regular};
+
+  @media ${device.mobileL} {
+    margin-top: ${space[4]};
+    font-size: ${p => (p.size === 'large' ? fontSize[18] : fontSize[16])};
+  }
+`
+
+const Text = styled.span`
+  ${textStyles};
+  font-size: ${p => (p.size === 'large' ? fontSize[14] : fontSize[12])};
+  opacity: 0.5;
+  margin-top: 0.125rem;
+
+  @media ${device.mobileL} {
+    margin-top: 0.375rem;
+    font-size: ${p => (p.size === 'large' ? fontSize[16] : fontSize[14])};
+  }
+`
+
+export function Card({
+  title,
+  subtitle,
+  text,
+  image,
+  leftAdornment,
+  rightAdornment,
+  size,
+  ...props
+}) {
+  const hasImage = Boolean(image)
+  const theme = hasImage ? 'light' : props.appearance
+
+  return (
+    <Container pad={!hasImage} size={size} {...props}>
+      {hasImage && <Image src={image} rounded />}
+      <Content floating={hasImage} alignItems={props.verticalAlign}>
+        {leftAdornment && <Header>{leftAdornment}</Header>}
+        <Body theme={theme}>
+          {title && <Title size={size}>{title}</Title>}
+          {subtitle && <Subtitle size={size}>{subtitle}</Subtitle>}
+          {text && (
+            <Text size={size} theme={theme}>
+              {text}
+            </Text>
+          )}
+        </Body>
+        {rightAdornment && <Footer>{rightAdornment}</Footer>}
+      </Content>
+    </Container>
+  )
+}
+
+Card.defaultProps = {
+  verticalAlign: 'center',
+  appearance: 'dark',
+  size: 'default',
+}
+
+Card.propTypes = {
+  title: PropTypes.string,
+  subtitle: PropTypes.string,
+  text: PropTypes.string,
+  image: PropTypes.string,
+  leftAdornment: PropTypes.element,
+  rightAdornment: PropTypes.element,
+  verticalAlign: PropTypes.oneOf(['top', 'center', 'bottom']),
+  appearance: PropTypes.oneOf(['dark', 'light']),
+  size: PropTypes.oneOf(['default', 'large']),
+}
