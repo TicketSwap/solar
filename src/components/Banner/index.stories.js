19aeaf814625ea9fccaa7966395e78845cd97112
Philipp Rappold <philipprappold@me.com>
2019-11-07 16:43:35 +0100

    refactor: remove deprecated components
    BREAKING CHANGE: removes BasicCard, Card, Carousel, Icon, TabsGroup and related components

--- src/components/Banner/index.stories.js
+++ src/components/Banner/index.stories.js
@@ -1,20 +1,17 @@
 import React from 'react'
 import { Banner } from './'
 import { H2 } from '../Heading'
-import { ContentWrapper } from '../BasicCarousel/index.stories'
 
 export default {
   title: 'Banner',
 }
 
 export const Basic = () => (
-  <ContentWrapper>
-    <Banner
-      backgroundImageUrl="https://mynoise.net/Data/RPGFOREST/fb.jpg"
-      url="https://ticketswap.com"
-    >
-      <H2>Some event</H2>
-      <p>Description about the event</p>
-    </Banner>
-  </ContentWrapper>
+  <Banner
+    backgroundImageUrl="https://mynoise.net/Data/RPGFOREST/fb.jpg"
+    url="https://ticketswap.com"
+  >
+    <H2>Some event</H2>
+    <p>Description about the event</p>
+  </Banner>
 )
