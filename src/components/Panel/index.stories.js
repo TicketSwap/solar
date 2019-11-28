7a0e62c99e4600278e776e5b83a8c7c836e38515
Roohn <ronald@ticketswap.com>
2019-11-28 12:20:38 +0100

    fix(Panel): remove overflow hidden

--- src/components/Panel/index.stories.js
+++ src/components/Panel/index.stories.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { H4 } from '../Heading'
 import { BaseButton } from '../BaseButton'
 import { Toggle } from '../Toggle'
+import { color } from '../../theme'
 import { Panel, PanelContent, PanelText, PanelBody, PanelFooter } from './'
 
 const Wrapper = story => <div style={{ padding: 24 }}>{story()}</div>
@@ -26,6 +27,31 @@ export const Single = () => (
 export const Group = () => (
   <Panel>
     <PanelContent>
+      <PanelBody>
+        <H4>Title</H4>
+        <PanelText>Lorem ipsum dolor sit amet</PanelText>
+        <BaseButton>Edit</BaseButton>
+      </PanelBody>
+      <PanelFooter>
+        <Toggle />
+      </PanelFooter>
+    </PanelContent>
+    <PanelContent>
+      <PanelBody>
+        <H4>Title</H4>
+        <PanelText>Lorem ipsum dolor sit amet</PanelText>
+        <BaseButton>Edit</BaseButton>
+      </PanelBody>
+      <PanelFooter>
+        <Toggle />
+      </PanelFooter>
+    </PanelContent>
+  </Panel>
+)
+
+export const WithSelected = () => (
+  <Panel>
+    <PanelContent bgColor={color.earthLightest}>
       <PanelBody>
         <H4>Title</H4>
         <PanelText>Lorem ipsum dolor sit amet</PanelText>
