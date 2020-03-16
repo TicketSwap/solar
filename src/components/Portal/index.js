79826f877301f2f99b6ddf4051a49e7cdd1f693c
RobVermeer <rob@vermeertech.nl>
2020-03-16 10:40:56 +0100

    fix(portal): the portal showed an error when server side rendered

--- src/components/Portal/index.js
+++ src/components/Portal/index.js
@@ -1,8 +1,16 @@
 import PropTypes from 'prop-types'
 import { createPortal } from 'react-dom'
 
+function canUseDOM() {
+  return Boolean(
+    typeof window !== 'undefined' &&
+      window.document &&
+      window.document.createElement
+  )
+}
+
 export const Portal = ({ selector, children }) => {
-  if (typeof document === 'undefined') return null
+  if (!canUseDOM()) return null
   const node = selector ? document.querySelector(selector) : document.body
   return node ? createPortal(children, node) : null
 }
