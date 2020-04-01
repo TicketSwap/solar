5a2561eabacd2ff94751f17839b127a05e050730
Philipp Rappold <philipprappold@me.com>
2020-04-01 14:08:08 +0200

    feat(Image): Add native lazy-loading through `gatsby-image`
    Instead of mimicking `gatsby-image` might as well use it right away.

--- src/components/Image/fetchImage.js
+++
@@ -1,8 +0,0 @@
-export function fetchImage(src) {
-  const image = new Image()
-  return new Promise((resolve, reject) => {
-    image.src = src
-    image.onload = resolve
-    image.onerror = reject
-  })
-}
