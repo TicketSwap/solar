1ed77e86eba97de83fc0b0fbceb4d013ace32a50
Philipp Rappold <philipprappold@me.com>
2020-06-05 11:41:40 +0200

    chore(Dialog): Remove unused import

--- src/components/Dialog/index.js
+++ src/components/Dialog/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from '@emotion/styled'
 import PropTypes from 'prop-types'
 import { Portal } from '../Portal'
