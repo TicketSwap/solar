4c52038866f5a60e781ad94c9a410ca014dcbeaa
Roohn <ronald@ticketswap.com>
2020-07-09 12:07:13 +0200

    feat(ContentDialog): Add support for logo images

--- src/components/ContentDialog/index.stories.js
+++ src/components/ContentDialog/index.stories.js
@@ -72,3 +72,30 @@ export const WithLongBody = () => {
     </>
   )
 }
+
+export const WithLogo = () => {
+  const [showDialog, setShowDialog] = React.useState(false)
+  const open = () => setShowDialog(true)
+  const close = () => setShowDialog(false)
+
+  return (
+    <>
+      <button onClick={open}>Open Dialog</button>
+      <ContentDialog
+        isOpen={showDialog}
+        onDismiss={close}
+        imageUrl="https://images.unsplash.com/photo-1532452119098-a3650b3c46d3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&h=600&q=80"
+        logoUrl="https://upload.wikimedia.org/wikipedia/commons/8/84/Example.svg"
+        title="Cracking TicketSwap"
+        subtitle="Insider tips for safely buying and selling tickets"
+      >
+        <H1>Heading 1</H1>
+        <p>
+          It’s no surprise the day I landed a job at TicketSwap an avalanche of
+          questions began about how to best get tickets? Things like; Do you get
+          free tickets for life now? Can you get me free tickets for life now?
+        </p>
+      </ContentDialog>
+    </>
+  )
+}
