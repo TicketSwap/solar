356df49f5b844fed24a092aef3be07cd10b0f79a
Glenn Gijsberts <glenngijsberts95@gmail.com>
2020-09-29 19:05:13 +0200

    perf(easingfunctions): easingFunctions are no longer used and therefore being removed
    BREAKING CHANGE: the easingFunctions are removed from the theme file.

--- src/theme.js
+++ src/theme.js
@@ -144,24 +144,6 @@ export const easing = {
   easeInOutCirc: 'cubic-bezier(.785, .135, .15, .86)',
 }
 
-export const easingFunctions = {
-  linear: t => t,
-  easeInQuad: t => t * t,
-  easeOutQuad: t => t * (2 - t),
-  easeInOutQuad: t => (t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t),
-  easeInCubic: t => t * t * t,
-  easeOutCubic: t => --t * t * t + 1,
-  easeInOutCubic: t =>
-    t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1,
-  easeInQuart: t => t * t * t * t,
-  easeOutQuart: t => 1 - --t * t * t * t,
-  easeInOutQuart: t => (t < 0.5 ? 8 * t * t * t * t : 1 - 8 * --t * t * t * t),
-  easeInQuint: t => t * t * t * t * t,
-  easeOutQuint: t => 1 + --t * t * t * t * t,
-  easeInOutQuint: t =>
-    t < 0.5 ? 16 * t * t * t * t * t : 1 + 16 * --t * t * t * t * t,
-}
-
 export const duration = 200
 export const transition = `${duration}ms ${easing.easeInOutCubic}`
 
