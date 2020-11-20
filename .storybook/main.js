cac9a4802a732b02364a4462a49ab2e345323384
Roohn <ronald@ticketswap.com>
2020-11-20 10:32:59 +0100

    chore: Fix emotion for storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,22 @@
+const path = require('path')
+const toPath = _path => path.join(process.cwd(), _path)
+
 module.exports = {
   stories: ['../src/**/*.stories.@(ts|js|tsx)'],
   addons: [],
   typescript: {
     check: true, // type-check stories during Storybook build
+  },
+  webpackFinal: async config => {
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        alias: {
+          ...config.resolve.alias,
+          '@emotion/styled': toPath('node_modules/@emotion/styled'),
+        },
+      },
+    }
   },
 }
