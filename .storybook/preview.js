a0a83478bf1833d5a2af2605b3072aab22ffb81d
Glenn Gijsberts <glenngijsberts95@gmail.com>
2020-12-28 11:31:26 +0100

    docs(storybook): add Theme switch to storybook preview
    This will implement a switch that will let you switch the preview from the default theme to the dark
    theme.

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { BaseStyles } from '../src/components/BaseStyles'
+import Theme from '../src/components/Theme'
 
 export const decorators = [
   Story => (
-    <div>
+    <Theme>
       <BaseStyles />
       <Story />
-    </div>
+    </Theme>
   ),
 ]
 
