99c0d70296b89da88a937464525bef8b1906aa9d
Glenn Gijsberts <glenngijsberts95@gmail.com>
2021-05-14 12:42:39 +0200

    feat(select): add right adornment to the select items

--- src/components/Select/index.stories.tsx
+++ src/components/Select/index.stories.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Select } from './'
 import { MagnifyingGlass } from '@ticketswap/comets'
+import { Flag } from '../Flag'
 
 const items = [
   { value: 'de', name: 'German' },
@@ -38,5 +39,28 @@ export const HideLabel = () => (
     initialSelectedItem={items[1]}
     leftAdornment={<MagnifyingGlass size={24} />}
     hideLabel
+  />
+)
+
+const languages = [
+  { value: 'de', name: 'German', rightAdornment: <Flag countryCode="de" /> },
+  { value: 'it', name: 'Italian', rightAdornment: <Flag countryCode="it" /> },
+  {
+    value: 'nl',
+    name: 'Dutch',
+    rightAdornment: <Flag countryCode="nl" />,
+    leftAdornment: <Flag countryCode="nl" />,
+  },
+]
+
+export const withItemAdornments = () => (
+  <Select
+    items={languages}
+    id="language"
+    label="Language"
+    help="Select a language"
+    onChange={selection => console.log(selection)}
+    initialSelectedItem={items[0]}
+    leftAdornment={<MagnifyingGlass size={24} />}
   />
 )
