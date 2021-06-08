519f5a3c7f4be6c5a19cdefabadec07858488525
Glenn Gijsberts <glenngijsberts95@gmail.com>
2021-06-08 14:00:12 +0200

    test(Panel): Add test

---
+++ src/components/Panel/index.test.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import { Panel, PanelContent, PanelText, PanelBody } from '.'
+
+describe('Panel', () => {
+  it('renders without crashing', () => {
+    const { container, getByText } = render(
+      <Panel>
+        <PanelContent>
+          <PanelBody>
+            <PanelText>Lorem ipsum dolor sit amet</PanelText>
+          </PanelBody>
+        </PanelContent>
+      </Panel>
+    )
+
+    expect(container.firstChild).toBeInTheDocument()
+    expect(getByText(/Lorem ipsum dolor sit amet/i)).toBeInTheDocument()
+  })
+})
