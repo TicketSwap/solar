93f3b70ceff35b9a8e8f33bec69a5cd27872f58f
Glenn Gijsberts <glenngijsberts95@gmail.com>
2021-06-08 13:10:26 +0200

    test(Tabs): Add test

---
+++ src/components/Tabs/index.test.tsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react'
+import { Tabs, TabList, Tab, TabPanels, TabPanel } from '.'
+
+const Content = () => (
+  <Tabs>
+    <TabList>
+      <Tab>Glenn</Tab>
+      <Tab>Samy</Tab>
+    </TabList>
+
+    <TabPanels>
+      <TabPanel>
+        <p>Frontend Developer</p>
+      </TabPanel>
+      <TabPanel>
+        <p>Backend Developer</p>
+      </TabPanel>
+    </TabPanels>
+  </Tabs>
+)
+
+describe('Tabs', () => {
+  it('renders without crashing', () => {
+    const { container } = render(<Content />)
+
+    expect(container.firstChild).toBeInTheDocument()
+  })
+
+  it('can select different tabs', () => {
+    const { container, getByText } = render(<Content />)
+
+    expect(getByText(/Glenn/i)?.getAttribute('aria-selected')).toEqual('true')
+
+    expect(getByText(/Samy/i)?.getAttribute('aria-selected')).toEqual('false')
+    expect(container.firstChild).toBeInTheDocument()
+
+    expect(
+      getByText(/Frontend Developer/i)
+        .closest('div')
+        ?.getAttribute('hidden')
+    ).toEqual(null)
+
+    fireEvent.click(getByText(/Samy/i))
+
+    expect(getByText(/Glenn/i)?.getAttribute('aria-selected')).toEqual('false')
+
+    expect(
+      getByText(/Backend Developer/i)
+        .closest('div')
+        ?.getAttribute('hidden')
+    ).toEqual(null)
+  })
+})
