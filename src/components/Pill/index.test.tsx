e6648a70291d615ccf325ed871973085b44c2f0b
Glenn Gijsberts <glenngijsberts95@gmail.com>
2021-06-08 13:28:54 +0200

    test(Pill): Add tests

---
+++ src/components/Pill/index.test.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import { matchers } from '@emotion/jest'
+import { Pill, PillVariant } from '.'
+import { color } from '../../theme'
+
+expect.extend(matchers)
+
+describe('Pill', () => {
+  it('renders without crashing', () => {
+    const { container, getByText } = render(<Pill>Primary</Pill>)
+    expect(container.firstChild).toBeInTheDocument()
+    expect(getByText(/Primary/i)).toBeInTheDocument()
+  })
+
+  it('renders a variant correctly', () => {
+    const { container, getByText } = render(
+      <Pill variant={PillVariant.success}>Success</Pill>
+    )
+    expect(container.firstChild).toHaveStyleRule(
+      'background-color',
+      color.titan
+    )
+    expect(getByText(/Success/i)).toHaveStyleRule('color', color.space)
+  })
+})
