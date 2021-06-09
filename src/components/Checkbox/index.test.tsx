51dd1b744f65e29fb4fe03bbb2af67dc54e56007
Glenn Gijsberts <glenngijsberts95@gmail.com>
2021-06-09 13:02:39 +0200

    test(Checkbox): Add tests

---
+++ src/components/Checkbox/index.test.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { Checkbox } from './'
+import { fireEvent, render } from '@testing-library/react'
+
+describe('Checkbox', () => {
+  it('renders without crashing', () => {
+    const { container } = render(
+      <Checkbox id="updates" label="Do you want to receive updates?" />
+    )
+    expect(container.firstChild).toBeInTheDocument()
+  })
+
+  it('can toggle the checkbox', () => {
+    const { getByLabelText, getByRole } = render(
+      <Checkbox id="updates" label="Do you want to receive updates?" />
+    )
+
+    fireEvent.click(getByLabelText(/updates/i))
+    expect(getByRole(/presentation/i)).toBeInTheDocument()
+  })
+
+  it('can be default on', () => {
+    const { getByRole } = render(
+      <Checkbox
+        id="updates"
+        label="Do you want to receive updates?"
+        defaultOn
+      />
+    )
+
+    expect(getByRole(/presentation/i)).toBeInTheDocument()
+  })
+
+  it('can have a help text', () => {
+    const { getByText } = render(
+      <Checkbox
+        id="updates"
+        label="Do you want to receive updates?"
+        help="Help me"
+      />
+    )
+
+    expect(getByText(/Help me/i)).toBeInTheDocument()
+  })
+})
