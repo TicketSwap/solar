9d5a55f38cd12384d0be22f68f5fb75a721bda3b
Glenn Gijsberts <glenngijsberts95@gmail.com>
2021-06-09 16:33:55 +0200

    test(OnboardingDialog): Add tests

---
+++ src/components/OnboardingDialog/index.test.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { StorybookDialog } from './index.stories'
+import { fireEvent, render } from '@testing-library/react'
+
+describe('OnboardingDialog', () => {
+  it('renders without crashing', () => {
+    const { container } = render(<StorybookDialog />)
+
+    expect(container.firstChild).toBeInTheDocument()
+  })
+
+  it('can close & open the dialog', () => {
+    const { getByText } = render(<StorybookDialog />)
+
+    expect(getByText(/Onboarding page/i)).toBeInTheDocument()
+    expect(getByText(/Onboarding you/i)).toBeInTheDocument()
+    fireEvent.click(getByText(/Next/i))
+    expect(getByText(/This explains everything/i)).toBeInTheDocument()
+    fireEvent.click(getByText(/Close/i))
+    fireEvent.click(getByText('Onboarding'))
+    expect(getByText(/Onboarding you/i)).toBeInTheDocument()
+  })
+})
