6e741b79fcbb4e08dbe2ffc4da58636a864fe987
Glenn Gijsberts <glenngijsberts95@gmail.com>
2021-06-09 14:53:34 +0200

    test(MoneyInput): Add tests

---
+++ src/components/MoneyInput/index.test.tsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react'
+import { MoneyInput } from '.'
+
+const currencies = [
+  {
+    code: 'EUR',
+    symbol: '€',
+    name: 'Euro',
+  },
+  {
+    code: 'DKK',
+    symbol: 'DKK',
+    name: 'Danish Krone',
+  },
+]
+
+describe('MoneyInput', () => {
+  it('renders without crashing', () => {
+    const { container } = render(
+      <MoneyInput
+        id="maximum-price"
+        label="Maximum price"
+        currencies={currencies}
+        currency={currencies[0]}
+        amount={25}
+      />
+    )
+
+    expect(container.firstChild).toBeInTheDocument()
+  })
+
+  it('can have a help text', () => {
+    const { getByText } = render(
+      <MoneyInput
+        id="maximum-price"
+        label="Maximum price"
+        currencies={currencies}
+        currency={currencies[0]}
+        amount={25}
+        help="The price you want to get"
+      />
+    )
+
+    expect(getByText('The price you want to get')).toBeInTheDocument()
+  })
+
+  it('can change the currency of the input', () => {
+    const { getByLabelText, getByText } = render(
+      <MoneyInput
+        id="maximum-price"
+        label="Maximum price"
+        currencies={currencies}
+        currency={currencies[0]}
+        amount={25}
+        help="The price you want to get"
+      />
+    )
+
+    expect(getByLabelText(/Currency/i)).toHaveValue('€')
+    fireEvent.focus(getByLabelText(/Currency/i))
+    fireEvent.mouseDown(getByText(/Danish Krone/i))
+    expect(getByLabelText(/Currency/i)).toHaveValue('DKK')
+  })
+})
