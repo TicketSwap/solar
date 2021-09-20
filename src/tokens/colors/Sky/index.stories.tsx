89bccd1d3e7e87103c8da9ea14010a1a8c527cf0
bobbonius <bobmuller.f@gmail.com>
2021-09-20 10:44:46 +0200

    docs(color): introduce story to display theme colors

---
+++ src/tokens/colors/Sky/index.stories.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { ColorComponent } from '../ColorComponent'
+
+export const Sky = () => <ColorComponent name="sky" />
+
+export default {
+  title: 'Design Tokens/Colors/Sky',
+}
---
+++ src/tokens/colors/Sun/index.stories.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { ColorComponent } from '../ColorComponent'
+
+export const Sun = () => <ColorComponent name="sun" />
+
+export default {
+  title: 'Design Tokens/Colors/Sun',
+}
