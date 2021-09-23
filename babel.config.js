22f07e86af413a6ea44ace1b7e18d5a105b4e39e
glenngijsberts <glenngijsberts95@gmail.com>
2021-09-23 17:11:17 +0200

    build: WIP

--- babel.config.js
+++ babel.config.js
@@ -12,7 +12,6 @@ module.exports = {
       },
     ],
     '@babel/preset-react',
-    '@babel/preset-typescript',
   ],
   plugins: [
     '@emotion',
---
+++ rollup.config.js
@@ -0,0 +1,40 @@
+import { babel } from '@rollup/plugin-babel'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
+import replace from '@rollup/plugin-replace'
+import visualizer from 'rollup-plugin-visualizer'
+import filesize from 'rollup-plugin-filesize'
+import pkg from './package.json'
+import { terser } from 'rollup-plugin-terser'
+import typescript from '@rollup/plugin-typescript'
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: pkg.main,
+      format: 'esm',
+    },
+  ],
+  plugins: [
+    replace({
+      preventAssignment: true,
+      'process.env.NODE_ENV': JSON.stringify('production'),
+      __buildDate__: () => JSON.stringify(new Date()),
+      __buildVersion: 15,
+    }),
+    peerDepsExternal(),
+    nodeResolve(),
+    babel({
+      exclude: 'node_modules/**',
+      babelHelpers: 'bundled',
+      presets: ['@babel/preset-env', '@babel/preset-react'],
+    }),
+    typescript({ tsconfig: './tsconfig.json' }),
+    terser(),
+    visualizer(),
+    filesize(),
+  ],
+  external: id =>
+    !id.startsWith('\0') && !id.startsWith('.') && !id.startsWith('/'),
+}
