9dd4b7ab55d775eff3be9f8f9d9851562d7ba9bf
Roohn <tol.ronald@gmail.com>
2021-10-15 13:51:20 +0200

    refactor: move component imports to own index inside folder

---
+++ src/components/index.ts
@@ -0,0 +1,74 @@
+export { Alert, AlertVariant, AlertAction } from './Alert'
+export { Avatar } from './Avatar'
+export {
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+} from './Accordion'
+export { Banner } from './Banner'
+export { Text, SmallText } from './Text'
+export { BaseStyles } from './BaseStyles'
+export { StarRating } from './StarRating'
+export { Button, ButtonVariant, ButtonSize } from './Button'
+export { CardCarousel } from './CardCarousel'
+export { Card, CardVerticalAlign, CardSize } from './Card'
+export { ContentDialog } from './ContentDialog'
+export { Panel, PanelContent, PanelBody, PanelText, PanelFooter } from './Panel'
+export { Pill, PillVariant } from './Pill'
+export { BaseButton } from './BaseButton'
+export { Collapsible } from './Collapsible'
+export { Toggle } from './Toggle'
+export { Checkbox } from './Checkbox'
+export { Select } from './Select'
+export { DateInput } from './DateInput'
+export { MoneyInput } from './MoneyInput'
+export { PhoneInput } from './PhoneInput'
+export { TimeInput } from './TimeInput'
+export { Combobox, SelectItem } from './Combobox'
+export { ContentTransition } from './ContentTransition'
+export { SkeletonPulse, SkeletonLine } from './Skeleton'
+export {
+  ToastProvider,
+  ToastConsumer,
+  Toast,
+  withToastContext,
+  ToastContext,
+  useToast,
+} from './Toast'
+export { Cover } from './Cover'
+export {
+  Dialog,
+  DialogWindow,
+  DialogHeader,
+  DialogAdornment,
+  DialogBody,
+  DialogFooter,
+  useDialog,
+} from './Dialog'
+export {
+  OnboardingDialog,
+  OnboardingDialogBody,
+  useOnboarding,
+  OnboardingWrapper,
+} from './OnboardingDialog'
+export { Flag } from './Flag'
+export { H1, H2, H3, H4, H5, H6 } from './Heading'
+export { HorizontalScroll } from './HorizontalScroll'
+export { Image } from './Image'
+export {
+  Input,
+  InputMenu,
+  InputMenuFooter,
+  InputMenuList,
+  InputMenuItem,
+  fieldStyles,
+} from './Input'
+export { Logo, LogoVariant } from './Logo'
+export { Menu, MenuList, MenuButton, MenuItem, MenuLink } from './MenuButton'
+export { Portal } from './Portal'
+export { SpeechBubble } from './SpeechBubble'
+export { Spinner } from './Spinner'
+export { Tabs, TabList, Tab, TabPanels, TabPanel } from './Tabs'
+export { VisuallyHidden } from './VisuallyHidden'
+export { Tooltip } from './Tooltip'
