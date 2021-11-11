17f45be88137cac8f0cde9bc336063fd5b4b9da7
bobbonius <bobmuller.f@gmail.com>
2021-11-11 17:49:33 +0100

    feat(onetimecodeinput): added a OneTimeCodeInput field
    This component is introduced to handle one time code password to, for example, verify a users
    phonenumber

---
+++ src/components/OneTimeCodeInput/index.stories.tsx
@@ -0,0 +1,103 @@
+import { OneTimeCodeInput } from './index'
+import {
+  Dialog,
+  DialogBody,
+  DialogFooter,
+  DialogHeader,
+  DialogWindow,
+} from '../Dialog'
+import { Button } from '../Button'
+
+export const Basic = () => {
+  return (
+    <Dialog on>
+      {({ getWindowProps }) => (
+        <>
+          <DialogWindow {...getWindowProps()}>
+            <DialogHeader>One Time Code Input</DialogHeader>
+            <DialogBody>
+              <OneTimeCodeInput
+                length={6}
+                id="otc-input"
+                label="Verificationcode"
+                helpText="Enter your 6 digit code here"
+                ariaLabel="One Time Code Input Field:"
+                validate={true}
+                onChange={value => console.log(value)}
+              />
+            </DialogBody>
+            <DialogFooter>
+              <Button onClick={() => console.log('submitted')} fullWidth>
+                Submit
+              </Button>
+            </DialogFooter>
+          </DialogWindow>
+        </>
+      )}
+    </Dialog>
+  )
+}
+
+export const ShortCode = () => {
+  return (
+    <Dialog on>
+      {({ getWindowProps }) => (
+        <>
+          <DialogWindow {...getWindowProps()}>
+            <DialogHeader>One Time Code Input</DialogHeader>
+            <DialogBody>
+              <OneTimeCodeInput
+                length={3}
+                id="otc-input"
+                label="Verificationcode"
+                helpText="Enter your 3 digit code here"
+                ariaLabel="One Time Code Input Field:"
+                validate={true}
+                onChange={value => console.log(value)}
+              />
+            </DialogBody>
+            <DialogFooter>
+              <Button onClick={() => console.log('submitted')} fullWidth>
+                Submit
+              </Button>
+            </DialogFooter>
+          </DialogWindow>
+        </>
+      )}
+    </Dialog>
+  )
+}
+
+export const LongCode = () => {
+  return (
+    <Dialog on>
+      {({ getWindowProps }) => (
+        <>
+          <DialogWindow {...getWindowProps()}>
+            <DialogHeader>One Time Code Input</DialogHeader>
+            <DialogBody>
+              <OneTimeCodeInput
+                length={8}
+                id="otc-input"
+                label="Verificationcode"
+                helpText="Enter your 8 digit code here"
+                ariaLabel="One Time Code Input Field:"
+                validate={true}
+                onChange={value => console.log(value)}
+              />
+            </DialogBody>
+            <DialogFooter>
+              <Button onClick={() => console.log('submitted')} fullWidth>
+                Submit
+              </Button>
+            </DialogFooter>
+          </DialogWindow>
+        </>
+      )}
+    </Dialog>
+  )
+}
+
+export default {
+  title: 'Components/Inputs/OneTimeCodeInput',
+}
---
+++ src/components/OneTimeCodeInput/index.test.tsx
@@ -0,0 +1,294 @@
+import React from 'react'
+import { fireEvent, render, screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { matchers } from '@emotion/jest'
+import { OneTimeCodeInput } from './'
+
+expect.extend(matchers)
+
+const fillOneTimeCode = async () => {
+  await screen.findByRole('textbox', { name: /onetimecode input field 1/i })
+  await screen.findByRole('textbox', { name: /onetimecode input field 2/i })
+  await screen.findByRole('textbox', { name: /onetimecode input field 3/i })
+  await screen.findByRole('textbox', { name: /onetimecode input field 4/i })
+  await screen.findByRole('textbox', { name: /onetimecode input field 5/i })
+}
+
+describe('OneTimeCodeInput', () => {
+  describe('when the length is equal to 6', () => {
+    it('renders a component with 6 inputs', () => {
+      render(
+        <OneTimeCodeInput
+          id="OneTimeCodeInput"
+          ariaLabel="OneTimeCode Input Field"
+          label="Verificationcode"
+          length={6}
+        />
+      )
+
+      expect(screen.getAllByRole('textbox')).toHaveLength(6)
+    })
+  })
+
+  describe('when the component has a label', () => {
+    it('renders the input with label', () => {
+      render(
+        <OneTimeCodeInput
+          id="OneTimeCodeInput"
+          length={5}
+          label="Verificationcode"
+          ariaLabel="OneTimeCode Input Field"
+        />
+      )
+
+      expect(screen.getByText(/Verificationcode/i))
+    })
+  })
+
+  describe('when the label is hidden', () => {
+    it('does not show the label', () => {
+      const { getByText } = render(
+        <OneTimeCodeInput
+          length={5}
+          id="OneTimeCodeInput"
+          ariaLabel="OneTimeCode Input Field"
+          label="Verificationcode"
+          hideLabel
+        />
+      )
+      const label = getByText(/Verificationcode/i).parentNode
+      expect(label).toHaveStyleRule('height', '1px')
+      expect(label).toHaveStyleRule('margin', '-1px')
+    })
+  })
+
+  describe('when an onChange event is triggered', () => {
+    it('handles the onChange event', async () => {
+      render(
+        <OneTimeCodeInput
+          id="OneTimeCodeInput"
+          length={5}
+          label="Verificationcode"
+          ariaLabel="OneTimeCode Input Field"
+        />
+      )
+
+      userEvent.type(
+        screen.getByRole('textbox', {
+          name: /onetimecode input field 1/i,
+        }),
+        '75689'
+      )
+
+      await fillOneTimeCode()
+
+      expect(
+        screen.getByRole('textbox', { name: /onetimecode input field 1/i })
+      ).toHaveDisplayValue(/7/i)
+      expect(
+        screen.getByRole('textbox', { name: /onetimecode input field 2/i })
+      ).toHaveDisplayValue(/5/i)
+      expect(
+        screen.getByRole('textbox', { name: /onetimecode input field 3/i })
+      ).toHaveDisplayValue(/6/i)
+      expect(
+        screen.getByRole('textbox', { name: /onetimecode input field 4/i })
+      ).toHaveDisplayValue(/8/i)
+      expect(
+        screen.getByRole('textbox', { name: /onetimecode input field 5/i })
+      ).toHaveDisplayValue(/9/i)
+    })
+  })
+
+  describe('when the values are pasted from the clipboard', () => {
+    it('pastes the values from the clipboard into the input fields', async () => {
+      render(
+        <OneTimeCodeInput
+          id="OneTimeCodeInput"
+          length={5}
+          label="Verificationcode"
+          ariaLabel="OneTimeCode Input Field"
+        />
+      )
+
+      fireEvent.change(
+        screen.getByRole('textbox', {
+          name: /onetimecode input field 1/i,
+        }),
+        { target: { value: '75689' } }
+      )
+
+      await fillOneTimeCode()
+
+      expect(
+        screen.getByRole('textbox', { name: /onetimecode input field 1/i })
+      ).toHaveDisplayValue(/7/i)
+      expect(
+        screen.getByRole('textbox', { name: /onetimecode input field 2/i })
+      ).toHaveDisplayValue(/5/i)
+      expect(
+        screen.getByRole('textbox', { name: /onetimecode input field 3/i })
+      ).toHaveDisplayValue(/6/i)
+      expect(
+        screen.getByRole('textbox', { name: /onetimecode input field 4/i })
+      ).toHaveDisplayValue(/8/i)
+      expect(
+        screen.getByRole('textbox', { name: /onetimecode input field 5/i })
+      ).toHaveDisplayValue(/9/i)
+    })
+  })
+
+  describe('when there is a navigation event', () => {
+    describe('and it is an ArrowLeft', () => {
+      it('moves left', async () => {
+        render(
+          <OneTimeCodeInput
+            id="OneTimeCodeInput"
+            length={5}
+            label="Verificationcode"
+            ariaLabel="OneTimeCode Input Field"
+          />
+        )
+
+        userEvent.type(
+          screen.getByRole('textbox', {
+            name: /onetimecode input field 1/i,
+          }),
+          '75689'
+        )
+
+        await fillOneTimeCode()
+
+        userEvent.type(
+          screen.getByRole('textbox', {
+            name: /onetimecode input field 5/i,
+          }),
+          '{arrowleft}'
+        )
+
+        expect(
+          screen.getByRole('textbox', {
+            name: /onetimecode input field 4/i,
+          })
+        ).toHaveFocus()
+      })
+    })
+
+    describe('and it is an ArrowRight', () => {
+      it('moves right', async () => {
+        render(
+          <OneTimeCodeInput
+            id="OneTimeCodeInput"
+            length={5}
+            label="Verificationcode"
+            ariaLabel="OneTimeCode Input Field"
+          />
+        )
+
+        userEvent.type(
+          screen.getByRole('textbox', {
+            name: /onetimecode input field 1/i,
+          }),
+          '75689'
+        )
+
+        await fillOneTimeCode()
+
+        userEvent.type(
+          screen.getByRole('textbox', {
+            name: /onetimecode input field 4/i,
+          }),
+          '{arrowright}'
+        )
+
+        expect(
+          screen.getByRole('textbox', {
+            name: /onetimecode input field 5/i,
+          })
+        ).toHaveFocus()
+      })
+    })
+
+    describe('and its a Backspace', () => {
+      describe('and there is a value', () => {
+        it('removes the value', async () => {
+          render(
+            <OneTimeCodeInput
+              id="OneTimeCodeInput"
+              length={5}
+              label="Verificationcode"
+              ariaLabel="OneTimeCode Input Field"
+            />
+          )
+
+          userEvent.type(
+            screen.getByRole('textbox', {
+              name: /onetimecode input field 1/i,
+            }),
+            '75689'
+          )
+
+          await fillOneTimeCode()
+
+          userEvent.type(
+            screen.getByRole('textbox', {
+              name: /onetimecode input field 5/i,
+            }),
+            '{backspace}'
+          )
+
+          waitFor(() => {
+            expect(
+              screen.getByRole('textbox', {
+                name: /onetimecode input field 5/i,
+              })
+            ).toHaveDisplayValue('')
+
+            expect(
+              screen.getByRole('textbox', {
+                name: /onetimecode input field 4/i,
+              })
+            ).toHaveDisplayValue('')
+          })
+        })
+      })
+
+      describe('and there is no value', () => {
+        it('moves focus to the previous input field', async () => {
+          render(
+            <OneTimeCodeInput
+              id="OneTimeCodeInput"
+              length={5}
+              label="Verificationcode"
+              ariaLabel="OneTimeCode Input Field"
+            />
+          )
+
+          userEvent.type(
+            screen.getByRole('textbox', {
+              name: /onetimecode input field 1/i,
+            }),
+            '7568'
+          )
+
+          await fillOneTimeCode()
+
+          userEvent.type(
+            screen.getByRole('textbox', {
+              name: /onetimecode input field 5/i,
+            }),
+            '{backspace}'
+          )
+
+          waitFor(() => {
+            expect(
+              screen.getByRole('textbox', {
+                name: /onetimecode input field 4/i,
+              })
+            ).toHaveFocus()
+          })
+        })
+      })
+    })
+  })
+})
