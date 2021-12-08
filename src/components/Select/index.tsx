a40557dc9e2536ed3ea2ba4e99087e535162e0e4
Diogo Redin <me@diogoredin.com>
2021-12-08 14:13:08 +0100

    feat: Auto scroll into selected option in Select component
    This commit changes the Select component so that when there is an option
    selected and the user opens the dropdown there is no need for the user
    to scroll down to see it.
    
    JIRA GIN-1719

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -204,6 +204,12 @@ export const Select: FC<SelectProps> = ({
     }
   }, [arrowUp, arrowDown, scrollHighlightedItemIntoView])
 
+  useEffect(() => {
+    if (isOpen) {
+      scrollHighlightedItemIntoView()
+    }
+  }, [isOpen, scrollHighlightedItemIntoView])
+
   const handleKeyDown = (e: KeyboardEvent) => {
     if (!isOpen) return false
 
