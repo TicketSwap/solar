01be0c23c7019831da93dfc8ab35057a3d28782a
Diogo Redin <me@diogoredin.com>
2021-12-13 12:58:51 +0100

    feat: Introduce DatePicker component
    This commit introduces a component that allows users to click it, see
    a calendar dialog and select a date.
    
    JIRA GIN-1719

---
+++ src/components/DatePicker/index.stories.tsx
@@ -0,0 +1,122 @@
+import React, { useState } from 'react'
+import DatePicker, { TimeFrame } from '.'
+
+export default {
+  title: 'Components/Inputs/DatePicker',
+}
+
+export const Simple = () => {
+  const [date, setDate] = useState(null)
+
+  return (
+    <DatePicker
+      date={date}
+      monthLabel="Month"
+      yearLabel="Year"
+      placeholder="Pick a date"
+      onChange={date => setDate(date)}
+    />
+  )
+}
+
+Simple.storyName = 'Without initial date'
+
+export const InitialDate = () => {
+  const [date, setDate] = useState(new Date())
+
+  return (
+    <DatePicker
+      date={date}
+      monthLabel="Month"
+      yearLabel="Year"
+      placeholder="Pick a date"
+      onChange={date => setDate(date)}
+    />
+  )
+}
+
+InitialDate.storyName = 'With initial date'
+
+export const Future = () => {
+  const [date, setDate] = useState(null)
+
+  return (
+    <DatePicker
+      date={date}
+      monthLabel="Month"
+      yearLabel="Year"
+      placeholder="Pick a date"
+      onChange={date => setDate(date)}
+    />
+  )
+}
+
+Future.storyName = 'Future dates'
+
+export const Past = () => {
+  const [date, setDate] = useState(null)
+
+  return (
+    <DatePicker
+      date={date}
+      timeFrame={TimeFrame.past}
+      monthLabel="Month"
+      yearLabel="Year"
+      placeholder="Pick a date"
+      onChange={date => setDate(date)}
+    />
+  )
+}
+
+Past.storyName = 'Past dates'
+
+export const All = () => {
+  const [date, setDate] = useState(null)
+
+  return (
+    <DatePicker
+      date={date}
+      timeFrame={TimeFrame.all}
+      monthLabel="Month"
+      yearLabel="Year"
+      placeholder="Pick a date"
+      onChange={date => setDate(date)}
+    />
+  )
+}
+
+All.storyName = 'All dates'
+
+export const WithTitle = () => {
+  const [date, setDate] = useState(null)
+
+  return (
+    <DatePicker
+      date={date}
+      title="Pick a date"
+      monthLabel="Month"
+      yearLabel="Year"
+      placeholder="Pick a date"
+      onChange={date => setDate(date)}
+    />
+  )
+}
+
+WithTitle.storyName = 'With title'
+
+export const WithInfo = () => {
+  const [date, setDate] = useState(null)
+
+  return (
+    <DatePicker
+      date={date}
+      info="Some extra info goes here"
+      monthLabel="Month"
+      yearLabel="Year"
+      placeholder="Pick a date"
+      onChange={date => setDate(date)}
+    />
+  )
+}
+
+WithInfo.storyName = 'With info'
---
+++ src/components/DatePicker/index.test.tsx
@@ -0,0 +1,127 @@
+import React from 'react'
+import { screen, render } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import DatePicker, { TimeFrame } from '.'
+
+describe('DatePicker', () => {
+  describe('when it has a placeholder set', () => {
+    it('renders the placeholder', async () => {
+      render(
+        <DatePicker
+          date={null}
+          placeholder="From"
+          title="Pick a date"
+          timeFrame={TimeFrame.future}
+          monthLabel="Month"
+          yearLabel="Year"
+          info="Some important info"
+          locale="en-EN"
+          onChange={jest.fn()}
+        />
+      )
+
+      expect(screen.getByText(/from/i)).toBeInTheDocument()
+    })
+  })
+
+  describe('when it has a date set', () => {
+    it('renders the date', async () => {
+      render(
+        <DatePicker
+          date={new Date(2020, 1, 1)}
+          placeholder="From"
+          title="Pick a date"
+          timeFrame={TimeFrame.future}
+          monthLabel="Month"
+          yearLabel="Year"
+          info="Some important info"
+          locale="en-EN"
+          onChange={jest.fn()}
+        />
+      )
+
+      expect(screen.getByText(/Feb 1, 2020/i)).toBeInTheDocument()
+    })
+  })
+
+  describe('when open', () => {
+    it('renders the calendar', async () => {
+      render(
+        <DatePicker
+          date={new Date(2020, 1, 1)}
+          placeholder="From"
+          title="Pick a date"
+          timeFrame={TimeFrame.future}
+          monthLabel="Month"
+          yearLabel="Year"
+          info="Some important info"
+          locale="en-EN"
+          onChange={jest.fn()}
+        />
+      )
+
+      userEvent.click(screen.getByText(/Feb 1, 2020/i))
+
+      const calendarTitle = await screen.findByText(/pick a date/i)
+
+      expect(calendarTitle).toBeInTheDocument()
+    })
+
+    describe('and a date is selected', () => {
+      beforeEach(() => {
+        jest.useFakeTimers('modern')
+        jest.setSystemTime(Date.parse('2020-11-18T00:00:00Z'))
+      })
+
+      afterEach(() => {
+        jest.useRealTimers()
+      })
+
+      it('calls the onChange with the selected date', async () => {
+        const onChange = jest.fn()
+
+        render(
+          <DatePicker
+            date={null}
+            placeholder="From"
+            title="Pick a date"
+            timeFrame={TimeFrame.future}
+            monthLabel="Month"
+            yearLabel="Year"
+            info="Some important info"
+            locale="en-EN"
+            onChange={onChange}
+          />
+        )
+
+        userEvent.click(screen.getByText(/From/i))
+
+        await screen.findByText(/pick a date/i)
+
+        userEvent.click(screen.getByText(/25/i))
+
+        expect(onChange).toHaveBeenCalledWith(new Date(2020, 10, 25))
+      })
+    })
+  })
+
+  describe('when in a different locale', () => {
+    it('renders the date correctly', async () => {
+      render(
+        <DatePicker
+          date={new Date(2020, 1, 1)}
+          placeholder="From"
+          title="Pick a date"
+          timeFrame={TimeFrame.future}
+          monthLabel="Month"
+          yearLabel="Year"
+          info="Some important info"
+          locale="nl-NL"
+          onChange={jest.fn()}
+        />
+      )
+
+      expect(screen.getByText(/1 feb. 2020/i)).toBeInTheDocument()
+    })
+  })
+})
