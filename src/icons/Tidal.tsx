ee83a2fd7bea60765a3a5cb8e83df9ae4a624154
glenngijsberts <glenngijsberts95@gmail.com>
2022-02-15 17:10:41 +0100

    feat(Icons): Add Tidal icon

---
+++ src/icons/Tidal.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { SvgWrapper } from './SvgWrapper'
+
+export const Tidal = (props: any) => (
+  <SvgWrapper a11yTitle="Tidal" {...props}>
+    <path d="m11.667 12.333-4.334 4.333L3 12.333 7.333 8l4.334 4.333Z" />
+    <path d="m20.333 12.333-4.332 4.333-4.334-4.333L16 8l4.333 4.333L24.667 8 29 12.333l-4.333 4.333-4.334-4.333ZM16 25.333l4.334-4.334L16 16.666l-4.333 4.333L16 25.333Z" />
+  </SvgWrapper>
+)
