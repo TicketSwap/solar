fe11b834252001bbb770ecd6d3c8b14bd1747bdb
Diogo Redin <me@diogoredin.com>
2022-02-16 14:31:23 +0100

    test: Introduce useDialog tests
    This commit introduces some simple tests to the useDialog hook.

--- src/components/Dialog/index.test.tsx
+++ src/components/Dialog/index.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { fireEvent, render } from '@testing-library/react'
+import { screen, render } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import { Button, ButtonVariant } from '../Button'
 import { Close } from '../../icons'
 import {
@@ -9,7 +10,27 @@ import {
   DialogFooter,
   DialogHeader,
   DialogWindow,
+  useDialog,
 } from './'
+
+const HooksDialog: React.FC = () => {
+  const { hide, getToggleProps, getWindowProps } = useDialog({
+    persist: false,
+  })
+
+  return (
+    <>
+      <Button {...getToggleProps()}>Show dialog</Button>
+      <DialogWindow {...getWindowProps()}>
+        <DialogHeader>Dialog Title</DialogHeader>
+        <DialogBody>Body</DialogBody>
+        <DialogFooter>
+          <Button onClick={hide}>Close</Button>
+        </DialogFooter>
+      </DialogWindow>
+    </>
+  )
+}
 
 const MyDialog: React.FC = ({ children, ...props }) => (
   <Dialog {...props}>
@@ -45,7 +66,7 @@ describe('Dialog', () => {
 
   it('renders overlay, window, header, body and footer when clicking the toggle', () => {
     const { getByText, getByTestId } = render(<MyDialog>Body</MyDialog>)
-    fireEvent.click(getByText(/show dialog/i))
+    userEvent.click(getByText(/show dialog/i))
     expect(getByTestId('dialog-overlay')).toBeInTheDocument()
     expect(getByTestId('dialog-content')).toBeInTheDocument()
     expect(getByText(/dialog title/i)).toBeInTheDocument()
@@ -57,8 +78,8 @@ describe('Dialog', () => {
     const { getByText, getByTestId, queryByTestId } = render(
       <MyDialog>Body</MyDialog>
     )
-    fireEvent.click(getByText(/show dialog/i))
-    fireEvent.click(getByTestId('dialog-overlay'))
+    userEvent.click(getByText(/show dialog/i))
+    userEvent.click(getByTestId('dialog-overlay'))
     expect(queryByTestId('dialog-content')).toBeNull()
   })
 
@@ -66,8 +87,39 @@ describe('Dialog', () => {
     const { getByText, getByTestId, queryByTestId } = render(
       <MyDialog>Body</MyDialog>
     )
-    fireEvent.click(getByText(/show dialog/i))
-    fireEvent.click(getByTestId('close-button'))
+    userEvent.click(getByText(/show dialog/i))
+    userEvent.click(getByTestId('close-button'))
     expect(queryByTestId('dialog-content')).toBeNull()
+  })
+
+  describe('useDialog', () => {
+    describe('when the toggle is pressed', () => {
+      it('opens the dialog', async () => {
+        render(<HooksDialog />)
+
+        userEvent.click(screen.getByRole('button', { name: /show dialog/i }))
+
+        const title = await screen.findByText(/Dialog title/i)
+        const body = await screen.findByText(/Body/i)
+
+        expect(title).toBeInTheDocument()
+        expect(body).toBeInTheDocument()
+      })
+    })
+
+    describe('when the close button is pressed', () => {
+      it('closes the dialog', async () => {
+        render(<HooksDialog />)
+
+        userEvent.click(screen.getByRole('button', { name: /show dialog/i }))
+
+        await screen.findByText(/Dialog title/i)
+
+        userEvent.click(screen.getByRole('button', { name: /close/i }))
+
+        expect(screen.queryByText(/Dialog title/i)).not.toBeInTheDocument()
+        expect(screen.queryByText(/Body/i)).not.toBeInTheDocument()
+      })
+    })
   })
 })
