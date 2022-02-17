723350e14d05977d61234334e87d67e7ed8db830
Diogo Redin <me@diogoredin.com>
2022-02-17 08:23:48 +0100

    refactor: Refactor useTransition hook and fix memory leaks
    This commit re-factors the useTransition hook. Important changes made:
    - The transitioning state is removed since we can use the "state" state
    to determine that;
    - rAF inside a rAF is not necessary if we useLayoutEffect. Contrary to
    useEffect, which fires after the browser has painted, useLayoutEffect
    fires before the painting phase of the browser. One rAF call is still
    necessary to schedule the layout update for the current frame.
    https://reactjs.org/docs/hooks-reference.html#uselayouteffect
    https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame
    https://blog.jakuba.net/request-animation-frame-and-use-effect-vs-use-layout-effect/
    - useCallback does not have any cleanup functions like useEffect, contrary
    to what we had in the code. Instead we now clear the timers and scheduled
    rAF when the component unmounts (needs an empty dependency array to
    fire only once and on unmount).

---
+++ src/hooks/useTransition/index.ts
@@ -0,0 +1,106 @@
+import {
+  useState,
+  useCallback,
+  useLayoutEffect,
+  useEffect,
+  useRef,
+} from 'react'
+import { usePrevious } from '../usePrevious'
+
+export const EXITED = 'exited'
+export const ENTERING = 'entering'
+export const ENTERED = 'entered'
+export const EXITING = 'exiting'
+
+export enum TransitionState {
+  EXITED = 'exited',
+  ENTERING = 'entering',
+  ENTERED = 'entered',
+  EXITING = 'exiting',
+}
+
+interface useTransitionParameters {
+  in: boolean
+  timeout: number
+  defaultTransitionState?: TransitionState
+  onExited?: Function
+  onEntering?: Function
+  onEntered?: Function
+  onExiting?: Function
+}
+
+export const useTransition = ({
+  in: on,
+  defaultTransitionState = TransitionState.EXITED,
+  timeout,
+  onExited = () => {},
+  onEntering = () => {},
+  onEntered = () => {},
+  onExiting = () => {},
+}: useTransitionParameters): [TransitionState, boolean, boolean] => {
+  const [state, setState] = useState(defaultTransitionState)
+  const [mounted, setMounted] = useState(false)
+
+  const prevOn = usePrevious(on)
+  const transitioning =
+    state === TransitionState.ENTERING || state === TransitionState.EXITING
+
+  const enteringTimerID = useRef<ReturnType<typeof setTimeout>>()
+  const exitingTimerID = useRef<ReturnType<typeof setTimeout>>()
+  const animationFrameID = useRef<number>()
+
+  const handleEntered = useCallback(() => {
+    setState(TransitionState.ENTERED)
+    onEntered()
+  }, [onEntered])
+
+  const handleEntering = useCallback(() => {
+    setState(TransitionState.ENTERING)
+    onEntering()
+
+    return setTimeout(handleEntered, timeout)
+  }, [timeout, onEntering, handleEntered])
+
+  const handleExited = useCallback(() => {
+    setState(TransitionState.EXITED)
+    onExited()
+
+    setMounted(false)
+  }, [onExited])
+
+  const handleExiting = useCallback(() => {
+    setState(TransitionState.EXITING)
+    onExiting()
+
+    return setTimeout(handleExited, timeout)
+  }, [onExiting, handleExited, timeout])
+
+  useLayoutEffect(() => {
+    if (transitioning) return
+
+    if (on && !prevOn) {
+      setMounted(true)
+
+      animationFrameID.current = requestAnimationFrame(() => {
+        enteringTimerID.current = handleEntering()
+      })
+    }
+
+    if (prevOn && !on) {
+      exitingTimerID.current = handleExiting()
+    }
+  }, [on, prevOn, handleEntering, transitioning, handleExiting])
+
+  useEffect(() => {
+    return () => {
+      if (typeof enteringTimerID.current !== 'undefined')
+        clearTimeout(enteringTimerID.current)
+      if (typeof exitingTimerID.current !== 'undefined')
+        clearTimeout(exitingTimerID.current)
+      if (typeof animationFrameID.current !== 'undefined')
+        cancelAnimationFrame(animationFrameID.current)
+    }
+  }, [])
+
+  return [state, mounted, transitioning]
+}
