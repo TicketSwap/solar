c168ccb43d6c372a8e378ecdd579c34bd1c05cdb
glenngijsberts <glenngijsberts95@gmail.com>
2022-03-31 14:52:29 +0200

    fix: use correct colors for tooltip

--- src/components/Tooltip/index.stories.tsx
+++ src/components/Tooltip/index.stories.tsx
@@ -1,21 +1,28 @@
+import styled from '@emotion/styled'
 import React from 'react'
 import { Tooltip } from '.'
+import { space } from '../../theme'
+import { Button } from '../Button'
 
 export default {
   title: 'Components/Data Display/Tooltip',
 }
 
+const Grid = styled.div`
+  display: flex;
+  gap: ${space[16]};
+`
+
 export const Basic = () => (
-  <>
+  <Grid>
     <Tooltip label="Lorem ipsum">
-      <button style={{ fontSize: 25 }}>
-        <span aria-hidden>🔔</span>
-      </button>
+      <Button>Enable</Button>
     </Tooltip>
+
     <Tooltip label="Dolor sit amet">
       <button style={{ fontSize: 25 }}>
         <span aria-hidden>🔔</span>
       </button>
     </Tooltip>
-  </>
+  </Grid>
 )
