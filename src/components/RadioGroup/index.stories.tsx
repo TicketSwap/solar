969ad89325acb3755da62c486fdebfcef73b3c7d
RobVermeer <rob@vermeertech.nl>
2022-06-20 11:31:22 +0200

    feat(Radio): Add Radio component
    Introducing the Radio component. This is the standalone modular component staying as close to the HTML semantics as possible.

---
+++ src/components/RadioGroup/index.stories.tsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import { RadioGroup } from '.'
+
+export default {
+  title: 'Components/Inputs/RadioGroup',
+}
+
+export const Basic = () => {
+  const [value, setValue] = useState('')
+
+  return (
+    <RadioGroup
+      legend="Who's the best?"
+      name="whos-the-best"
+      options={[
+        { id: 'Emeline', value: 'Emeline', label: 'Emeline' },
+        { id: 'Diogo', value: 'Diogo', label: 'Diogo' },
+        { id: 'Ronald', value: 'Ronald', label: 'Ronald' },
+        { id: 'Glenn', value: 'Glenn', label: 'Glenn' },
+        { id: 'Rob', value: 'Rob', label: 'Rob', disabled: true },
+        { id: 'Bob', value: 'Bob', label: 'Bob' },
+        { id: 'Viktor', value: 'Viktor', label: 'Viktor' },
+      ]}
+      value={value}
+      onChange={event => {
+        console.log(event)
+        setValue(event.target.value)
+      }}
+    />
+  )
+}
