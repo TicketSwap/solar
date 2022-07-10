b8a3504546f8f3cab87b6353d141c996825953b7
glenngijsberts <glenngijsberts95@gmail.com>
2022-07-10 20:52:41 +0200

    feat: add Bookmark icon

---
+++ src/icons/Bookmark.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { SvgWrapper } from './SvgWrapper'
+
+export const Bookmark = (props: any) => (
+  <SvgWrapper a11yTitle="Bookmark" {...props}>
+    <path d="M4 3a2 2 0 0 1 2-2h20a2 2 0 0 1 2 2v27.41c0 .915-1.126 1.35-1.74.673L16 19.783l-10.26 11.3c-.614.677-1.74.242-1.74-.672V3Z" />
+  </SvgWrapper>
+)
