deaf737f6e5d25d37b6682849c43188ea86973ed
viktorrenkema <vrenkema@gmail.com>
2022-07-21 09:40:11 +0200

    Allow for passing path fill as prop

--- src/icons/Clock.tsx
+++ src/icons/Clock.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
-import { SvgWrapper } from './SvgWrapper'
+import { color as themeColor } from '../theme'
+import { SvgWrapper, SvgWrapperProps } from './SvgWrapper'
 
-export const Clock = (props: any) => (
-  <SvgWrapper a11yTitle="Clock" {...props}>
-    <path d="M14 0.666992C21.732 0.666992 28 6.93501 28 14.667C28 22.399 21.732 28.667 14 28.667C6.26801 28.667 0 22.399 0 14.667C0 6.93501 6.26801 0.666992 14 0.666992ZM14 4.66699C12.8954 4.66699 12 5.56242 12 6.66699V14.667C12 15.7716 12.8954 16.667 14 16.667H20C21.1046 16.667 22 15.7716 22 14.667C22 13.5624 21.1046 12.667 20 12.667H16V6.66699C16 5.56242 15.1046 4.66699 14 4.66699Z" />
+interface ClockProps {
+  accentColor?: string
+}
+
+export const Clock = ({
+  accentColor = themeColor.background,
+  ...props
+}: SvgWrapperProps & ClockProps) => (
+  <SvgWrapper {...props}>
+    <path d="M14,0 C21.732,0 28,6.268018 28,14 C28,21.732008 21.732,28 14,28 C6.26801,28 0,21.732008 0,14 C0,6.268018 6.26801,0 14,0 Z"></path>
+    <path
+      d="M14,3.999998 C12.8954,3.999998 12,4.895428 12,5.999998 L12,13.999998 C12,15.104608 12.8954,15.999998 14,15.999998 L20,15.999998 C21.1046,15.999998 22,15.104608 22,13.999998 C22,12.895408 21.1046,11.999998 20,11.999998 L16,11.999998 L16,5.999998 C16,4.895428 15.1046,3.999998 14,3.999998 Z"
+      fill={accentColor}
+    ></path>
   </SvgWrapper>
 )
