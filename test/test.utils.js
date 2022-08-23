f64fedf67539a093a1cb2745803e96178e1ac405
Diogo Redin <me@diogoredin.com>
2022-08-23 16:57:02 +0200

    chore: Introduce test utils to test file uploads
    This commit introduces two utils that allow us to test file uploads.
    See the dataTransfer section here for more details:
    https://testing-library.com/docs/dom-testing-library/api-events/
    
    JIRA HG-2167

--- test/test.utils.js
+++ test/test.utils.js
@@ -1,5 +1,35 @@
-import { render } from '@testing-library/react'
+import { render, waitFor } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
+import { act } from 'react-dom/test-utils'
+
+export const createFileForDataTransfer = (name, size, type) => {
+  const file = new File([], name, { type })
+  Object.defineProperty(file, 'size', {
+    get() {
+      return size * 1024 ** 2
+    },
+  })
+  return file
+}
+
+export const createDataTransferWithFiles = (files = []) => {
+  return {
+    dataTransfer: {
+      files,
+      items: files.map(file => ({
+        kind: 'file',
+        size: file.size,
+        type: file.type,
+        getAsFile: () => file,
+      })),
+      types: ['Files'],
+    },
+  }
+}
+
+export const flushPromises = async (rerender, ui) => {
+  await act(() => waitFor(() => rerender(ui)))
+}
 
 export const customRender = (ui, options) => ({
   user: userEvent.setup({
