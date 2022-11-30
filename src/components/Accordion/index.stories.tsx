2a55529e23872a67f66acebfbf32497ac603dae3
glenngijsberts <glenngijsberts95@gmail.com>
2022-11-30 18:06:45 +0100

    docs(Accordion): Add example of a controlled accordion

--- src/components/Accordion/index.stories.tsx
+++ src/components/Accordion/index.stories.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   Accordion,
   AccordionItem,
@@ -67,6 +67,36 @@ export const Collapsible = () => (
     </AccordionItem>
   </Accordion>
 )
+
+export const Controlled = () => {
+  const [index, setIndex] = useState(1)
+
+  return (
+    <Accordion index={index} onChange={setIndex} collapsible>
+      <AccordionItem>
+        <AccordionButton>Do a thing</AccordionButton>
+        <AccordionPanel>
+          <p>
+            Here are some detailed instructions about doing a thing. I am very
+            complex and probably contain a lot of content, so a user can hide or
+            show me by clicking the button above.
+          </p>
+        </AccordionPanel>
+      </AccordionItem>
+      <AccordionItem>
+        <AccordionButton>Do another thing</AccordionButton>
+        <AccordionPanel>
+          <p>
+            Here are some detailed instructions about doing yet another thing.
+            There are a lot of things someone might want to do, so I am only
+            going to talk about doing that other thing. I'll let my fellow
+            accordion items go into detail about even more things.
+          </p>
+        </AccordionPanel>
+      </AccordionItem>
+    </Accordion>
+  )
+}
 
 export default {
   title: 'Components/Surfaces/Accordion',
