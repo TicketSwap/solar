9e4cde984e19a1eea91e7195c862a38d8dd767e0
Glenn Gijsberts <glenngijsberts95@gmail.com>
2023-04-24 10:12:24 +0200

    feat(Cover): Introduce new cover

---
+++ src/components/CoverV2/Cover.tsx
@@ -0,0 +1,206 @@
+import React, { ReactElement } from 'react'
+import styled from '@emotion/styled'
+import {
+  color,
+  device,
+  space,
+  fontSize,
+  fontWeight,
+  lineHeight,
+} from '../../theme'
+import { H1, H2 } from '../Heading'
+import { css } from '@emotion/react'
+
+interface CoverContainerProps {
+  fullHeight: boolean
+}
+
+const CoverContainer = styled.div<CoverContainerProps>`
+  position: relative;
+  inline-size: 100%;
+  block-size: auto;
+  overflow: hidden;
+  background-color: var(--_coverBackgroundColor, ${color.darkBackground});
+
+  ${props =>
+    props.fullHeight &&
+    css`
+      @media ${device.tablet} {
+        min-block-size: 100vh;
+        display: flex;
+        align-items: flex-end;
+      }
+    `}
+`
+
+export interface CoverImages {
+  desktop?: string
+  tablet?: string
+  mobilePortrait?: string
+  mobileLandscape?: string
+}
+
+interface CoverBackgroundImageProps {
+  images?: CoverImages
+}
+
+const CoverBackgroundImage = styled.div<CoverBackgroundImageProps>`
+  position: absolute;
+  inset-inline: 0;
+  inset-block-start: 0;
+  block-size: 100%;
+  background-size: cover;
+  background-position: center;
+  filter: blur(15px);
+  transform: scale(1.2);
+
+  ${props =>
+    props.images &&
+    props.images.mobileLandscape &&
+    css`
+      background-image: url(${props.images.mobileLandscape});
+    `};
+
+  ${props =>
+    props.images &&
+    props.images.mobilePortrait &&
+    css`
+      @media ${device.mobileM} and (orientation: portrait) {
+        background-image: url(${props.images.mobilePortrait});
+      }
+    `};
+
+  ${props =>
+    props.images &&
+    props.images.tablet &&
+    css`
+      @media ${device.tablet} and (orientation: portrait) {
+        background-image: url(${props.images.tablet});
+      }
+    `};
+
+  ${props =>
+    props.images &&
+    props.images.desktop &&
+    css`
+      @media ${device.tablet} {
+        background-image: url(${props.images.desktop});
+      }
+    `};
+
+  &:after {
+    content: '';
+    position: absolute;
+    block-size: 100%;
+    inline-size: 100%;
+    inset-inline: 0;
+    inset-block-start: 0;
+    background: linear-gradient(
+      180deg,
+      rgba(26, 33, 41, 0.6) 0%,
+      rgba(26, 33, 41, 0.8) 100%
+    );
+  }
+`
+
+const CoverContent = styled.div`
+  position: relative;
+  margin-block: 0;
+  margin-inline: auto;
+  padding-inline-start: ${space[16]};
+  padding-inline-end: ${space[16]};
+  padding-block: ${space[48]};
+  max-inline-size: ${space[768]};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  text-align: center;
+
+  @media ${device.tablet} {
+    padding-block: ${space[64]};
+    padding-inline-start: ${space[32]};
+    padding-inline-end: ${space[32]};
+  }
+`
+
+const Titles = styled.div`
+  + * {
+    margin-block-start: ${space[16]};
+  }
+`
+
+const Title = styled(H1)`
+  color: var(--_coverTitleColor, ${color.lightForeground});
+`
+
+const Subtitle = styled(H2)`
+  color: var(--_coverSubtitleColor, ${color.lightForegroundMuted});
+  font-weight: ${fontWeight.regular};
+  font-size: ${fontSize[20]};
+  line-height: ${lineHeight.title};
+
+  @media ${device.tablet} {
+    font-size: ${fontSize[24]};
+  }
+`
+
+const Graphic = styled.div`
+  margin-block-end: ${space[16]};
+
+  @media ${device.tablet} {
+    margin-block-end: ${space[32]};
+  }
+`
+
+const MetaInfo = styled.div`
+  + * {
+    margin-block-start: ${space[16]};
+  }
+
+  @media ${device.tablet} {
+    + * {
+      margin-block-start: ${space[32]};
+    }
+  }
+`
+
+interface CoverProps {
+  title: string
+  subtitle?: string
+  images?: CoverImages
+  graphic?: ReactElement
+  metaInfo?: ReactElement
+  actions?: ReactElement
+  fullHeight?: boolean
+}
+
+export const Cover = ({
+  images,
+  title,
+  subtitle,
+  graphic,
+  metaInfo,
+  actions,
+  fullHeight = false,
+  ...props
+}: CoverProps) => {
+  return (
+    <CoverContainer {...props} fullHeight={fullHeight}>
+      <CoverBackgroundImage images={images} />
+
+      <CoverContent>
+        {graphic && <Graphic>{graphic}</Graphic>}
+
+        <Titles>
+          <Title>{title}</Title>
+          {subtitle && <Subtitle>{subtitle}</Subtitle>}
+        </Titles>
+
+        {metaInfo && <MetaInfo>{metaInfo}</MetaInfo>}
+
+        {actions && actions}
+      </CoverContent>
+    </CoverContainer>
+  )
+}
---
+++ src/components/CoverV2/index.stories.tsx
@@ -0,0 +1,219 @@
+import React from 'react'
+import { Cover } from '.'
+import styled from '@emotion/styled'
+import { color, device, radius, space } from '../../theme'
+import {
+  Calendar,
+  ChevronRightAlt,
+  Clock,
+  MusicalNote,
+  PlusAlt,
+  Tag,
+} from '../../icons'
+import { Button } from '../Button'
+
+const Graphic = styled.div`
+  block-size: ${space[64]};
+  inline-size: ${space[64]};
+  background-color: ${color.lightBackground};
+  opacity: 0.12;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  @media ${device.tablet} {
+    block-size: ${space[96]};
+    inline-size: ${space[96]};
+  }
+`
+
+const Image = styled.img`
+  aspect-ratio: 16 / 9;
+  border-radius: ${radius.lg};
+  max-width: 375px;
+`
+
+const StyledCover = styled(Cover)`
+  --_coverTitleColor: ${color.darkForeground};
+  --_coverSubtitleColor: ${color.darkForeground};
+  --_coverBackgroundColor: ${color.earth};
+`
+
+const Actions = styled.div`
+  display: flex;
+`
+
+const List = styled.ul`
+  display: flex;
+  gap: ${space[16]};
+
+  li {
+    a,
+    p {
+      color: ${color.lightForegroundMuted};
+    }
+  }
+`
+
+export const basic = () => (
+  <Cover
+    title="Pop"
+    subtitle="Discover our pop content"
+    images={{
+      desktop:
+        'https://cdn.ticketswap.com/public/202210/07bd30f6-0cea-4f06-9cb4-fd2e9eff4f2a.jpg',
+    }}
+  />
+)
+
+export const withIconGraphic = () => (
+  <Cover
+    title="Pop"
+    subtitle="Discover our pop content"
+    images={{
+      desktop:
+        'https://cdn.ticketswap.com/public/202210/07bd30f6-0cea-4f06-9cb4-fd2e9eff4f2a.jpg',
+    }}
+    graphic={
+      <Graphic>
+        <Tag size={40} />
+      </Graphic>
+    }
+  />
+)
+
+export const withImageGraphic = () => (
+  <Cover
+    title="Pop"
+    subtitle="Discover our pop content"
+    images={{
+      desktop:
+        'https://cdn.ticketswap.com/public/202211/zwarte-cross-2023-zwarte-cross-20-july-2023-1667298561.image.jpeg',
+    }}
+    graphic={
+      <Image
+        src="https://cdn.ticketswap.com/public/202211/zwarte-cross-2023-zwarte-cross-20-july-2023-1667298561.image.jpeg"
+        alt="Pop image"
+      />
+    }
+  />
+)
+
+export const withMetaInfo = () => (
+  <Cover
+    title="Pop"
+    subtitle="Discover our pop content"
+    images={{
+      desktop:
+        'https://cdn.ticketswap.com/public/202210/07bd30f6-0cea-4f06-9cb4-fd2e9eff4f2a.jpg',
+    }}
+    metaInfo={
+      <List>
+        <li>
+          <p>
+            <PlusAlt size={16} />
+            100 artists
+          </p>
+        </li>
+        <li>
+          <a href="/Pop">
+            Pop genre <ChevronRightAlt size={16} />
+          </a>
+        </li>
+      </List>
+    }
+    graphic={
+      <Graphic>
+        <Tag size={40} />
+      </Graphic>
+    }
+  />
+)
+
+export const withActions = () => (
+  <Cover
+    title="Adele"
+    subtitle="100 followers"
+    images={{
+      desktop:
+        'https://cdn.ticketswap.com/public/202210/07bd30f6-0cea-4f06-9cb4-fd2e9eff4f2a.jpg',
+    }}
+    metaInfo={
+      <List>
+        <li>
+          <p>
+            <PlusAlt size={16} />
+            100 artists
+          </p>
+        </li>
+        <li>
+          <a href="/Pop">
+            Pop genre <ChevronRightAlt size={16} />
+          </a>
+        </li>
+      </List>
+    }
+    actions={
+      <Actions>
+        <Button size="small">Follow me</Button>,
+        <Button size="small" variant="secondary">
+          Follow me
+        </Button>
+      </Actions>
+    }
+    graphic={
+      <Graphic>
+        <MusicalNote size={40} />
+      </Graphic>
+    }
+  />
+)
+
+export const withFullHeight = () => (
+  <Cover
+    title="How to make a cover"
+    subtitle="A blog post about making a cover"
+    fullHeight
+    images={{
+      desktop:
+        'https://cdn.ticketswap.com/public/202210/07bd30f6-0cea-4f06-9cb4-fd2e9eff4f2a.jpg',
+    }}
+    metaInfo={
+      <List>
+        <li>
+          <p>
+            <Clock size={16} /> 1 minute read
+          </p>
+        </li>
+        <li>
+          <p>
+            <Calendar size={16} /> 28/11
+          </p>
+        </li>
+      </List>
+    }
+  />
+)
+
+export const withOtherColors = () => (
+  <StyledCover
+    title="Pop"
+    subtitle="Discover our pop content"
+    graphic={
+      <Graphic>
+        <Tag size={40} />
+      </Graphic>
+    }
+  />
+)
+
+export const withoutBackground = () => (
+  <Cover title="Awakenings Festival 2026" />
+)
+
+const story = {
+  title: 'Components/Content/CoverV2',
+}
+
+export default story
